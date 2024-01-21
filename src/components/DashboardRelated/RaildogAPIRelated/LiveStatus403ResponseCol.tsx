803a5173d78d26ddc1bb49242cb1b304589611a3
Assassin <moinak.dey8@gmail.com>
2024-01-21 23:46:23 +0530

    Modify the heading and each status description, add respective  color to each status code

--- src/components/DashboardRelated/RaildogAPIRelated/LiveStatus403ResponseCol.tsx
+++ src/components/DashboardRelated/RaildogAPIRelated/LiveStatus403ResponseCol.tsx
@@ -1,11 +1,11 @@
 export default function LiveStatus403ResponseCol() {
   return (
     <tr>
-      <td className="py-2 px-4 white-border-all">403</td>
+      <td className="py-2 px-4 white-border-all text-red-500">403</td>
       <td className="w-[70%] py-2 px-4 white-border-all">
-        Bad response. This response is received when the server encounters a captcha verification
-        while scrapping for information. It triggers an authentication. You must call the 'Bypass
-        Captcha API' to get a new 'phpsessid'.
+        Bad response. It means the server encountered a captcha verification while scrapping for
+        information. It triggers an authentication. You must call the 'Bypass Captcha API' to get a
+        new 'phpsessid'.
       </td>
     </tr>
   );
--- src/components/DashboardRelated/RaildogAPIRelated/StatusChart.tsx
+++ src/components/DashboardRelated/RaildogAPIRelated/StatusChart.tsx
@@ -8,7 +8,7 @@ export default function StatusChart({ column }: Props) {
   return (
     <>
       <div className="py-4 px-2 white-border">
-        <h2>Response Status</h2>
+        <h2>What does the response mean?</h2>
       </div>
       <table className="table-fixed border-separate border-spacing-2">
         <thead>
@@ -19,22 +19,21 @@ export default function StatusChart({ column }: Props) {
         </thead>
         <tbody className="font-semibold">
           <tr>
-            <td className="py-2 px-4 white-border-all">200</td>
+            <td className="py-2 px-4 white-border-all text-green-500">200</td>
             <td className="py-2 px-4 white-border-all">
-              The response is good and you get the requested data.
+              Good response. You get all the requested data.
             </td>
           </tr>
           <tr>
-            <td className="py-2 px-4 white-border-all">400</td>
+            <td className="py-2 px-4 white-border-all text-red-500">400</td>
             <td className="w-[70%] py-2 px-4 white-border-all">
-              The response is bad. Check all the details before sending a request.
+              Bad response. Check all the details before sending a request.
             </td>
           </tr>
           <tr>
-            <td className="py-2 px-4 white-border-all">500</td>
+            <td className="py-2 px-4 white-border-all text-red-500">500</td>
             <td className="w-[70%] py-2 px-4 white-border-all">
-              The response is bad due to an error originating in the server. You should try again
-              after sometime.
+              Bad response. There was an error in the server. Try again after sometime.
             </td>
           </tr>
           {column && column}
